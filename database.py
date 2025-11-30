b4a3f246ef6b3cde60b7402d351710e2af92b3b2
skeswarsudhan <skeswarsudhan@gmail.com>
2025-11-30 12:27:11 +0530

    updated database connection 30112025 1227

--- database.py
+++ database.py
@@ -10,7 +10,11 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL is not set")
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"}
+)
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
